8ca60af5a7f6798afbb908ec1fe0506e1c041c02
Coolicky <piotr.kulicki@gmail.com>
2022-01-24 12:23:29 +0000

    Testing with Serilog.

--- Monitoring.Revit/App.cs
+++ Monitoring.Revit/App.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using Autodesk.Revit.DB;
+using System.Reflection;
 using Autodesk.Revit.DB.Events;
 using Autodesk.Revit.UI;
-using Monitoring.Revit.Commands.Availability;
-using Monitoring.Revit.Commands.HelloWorld;
-using Monitoring.Revit.Commands.SampleInjection;
-using Monitoring.Revit.Commands.SampleWindows;
+using Microsoft.Extensions.Configuration;
 using Revit.DependencyInjection.Unity.Applications;
-using Revit.DependencyInjection.Unity.Async;
 using Revit.DependencyInjection.Unity.Base;
-using Revit.DependencyInjection.Unity.UI;
+using Serilog;
+using Serilog.Formatting.Json;
 using Unity;
 using Unity.Lifetime;
 
@@ -23,44 +17,35 @@ namespace Monitoring.Revit
     [ContainerProvider("91EA445D-CED3-48AD-BBC8-0CB2844E1A80")]
     public class App : RevitApp
     {
-        public override void OnCreateRibbon(IRibbonManager ribbonManager)
-        {
-            // var br = ribbonManager.GetLineBreak();
-            //
-            // var sampleTab = ribbonManager.CreateTab("Sample Tab");
-            // var samplePanel = ribbonManager.CreatePanel(sampleTab.GetTabName(), "Sample Panel");
-            //
-            // samplePanel.AddPushButton<HelloWorldCommand, AvailableAlways>($"Hello{br}World", "hello");
-            // samplePanel.AddPushButton<SampleInjectionCommand, AvailableOnProject>($"Get{br}Selection", "selection");
-            // samplePanel.AddPushButton<SampleWindowCommand, AvailableOnProject>($"Show{br}Window", "window");
-        }
-        
-        public static void AddTimeStamp(string text)
-        {            
-            using (var streamWriter = new StreamWriter("C:\\Logs\\log.txt",true))
-            {
-                var builder = new StringBuilder();
-                builder.Append($" MachineName:{Environment.MachineName}");
-                builder.Append($" UserName:{Environment.UserName}");
-                builder.Append($" Domain:{Environment.UserDomainName}");
-                builder.Append($" OperatingSystem:{Environment.OSVersion}");
-                streamWriter.WriteLine($"{DateTime.Now:O} -> {text}{builder}");
-            }
-        }
-
         private static IUnityContainer UnityContainer { get; set; }
         public override Result OnStartup(IUnityContainer container, UIControlledApplication application)
         {
-            // container.AddRevitAsync(GetAsyncSettings);
-            // container.RegisterSampleServices();
-            // container.RegisterViews();
-            // container.RegisterViewModels();
-            
-            
-            
             UnityContainer = container;
-            AddTimeStamp("Startup");
+            var app = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(app, "appSettings.json"));
+            var config = configurationBuilder.Build();
             
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File(new JsonFormatter(",\n"),"C:\\Logs\\log.json")
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("machineName", Environment.MachineName)
+                .Enrich.WithProperty("userName", Environment.UserName)
+                .Enrich.WithProperty("domain", Environment.UserDomainName)
+                .Enrich.WithProperty("operatingSystem", Environment.OSVersion)
+                .Enrich.WithProperty("revitVersionName", application.ControlledApplication.VersionName)
+                .Enrich.WithProperty("revitVersion", application.ControlledApplication.VersionNumber)
+                .Enrich.WithProperty("revitVersion", application.ControlledApplication.VersionBuild)
+                .Enrich.WithProperty("revitType", application.ControlledApplication.Product.ToString())
+                .Enrich.WithProperty("revitLanguage", application.ControlledApplication.Language.ToString())
+                .CreateLogger();
+            
+            
+            
+            
+            Log.Information("Revit Started");
+
             application.ControlledApplication.ApplicationInitialized += Initialized;
             application.ControlledApplication.DocumentOpening += DocOpening;
             application.ControlledApplication.DocumentOpened += DocOpened;
@@ -71,8 +56,9 @@ namespace Monitoring.Revit
         {
             
             var timer = UnityContainer.Resolve<ITimer>("DocumentOpening");
+            timer.AddArgs("Document", e.Document.PathName);
             timer.Stop();
-            AddTimeStamp("Opened");
+            Log.Information("Revit Document {Document} Opened", e.Document.PathName);
         }
 
         private void DocOpening(object sender, DocumentOpeningEventArgs e)
@@ -86,63 +72,18 @@ namespace Monitoring.Revit
             timer.Start();
             UnityContainer.RegisterInstance(typeof(ITimer), "DocumentOpening", timer , new SingletonLifetimeManager());
             
-            AddTimeStamp("Opening");
+            Log.Information("{Document} Opening Started", e.DocumentType);
         }
 
         private void Initialized(object sender, ApplicationInitializedEventArgs e)
         {
-            AddTimeStamp("Initialized");
+            Log.Information("Revit Application Initialized");
         }
         
         public override Result OnShutdown(IUnityContainer container, UIControlledApplication application)
         {
+            Log.CloseAndFlush();
             return Result.Succeeded;
-        }
-
-        private void GetAsyncSettings(RevitAsyncSettings settings)
-        {
-            settings.Name = "Revit DI Samples";
-            settings.IsJournalable = true;
-        }
-    }
-
-    public interface ITimer
-    {
-        Stopwatch Stopwatch { get; set; }
-        void Start();
-        void Stop();
-        void Log();
-    }
-
-    public class Timer : ITimer
-    {
-        private readonly Dictionary<string, object> _args;
-
-        public Timer(Dictionary<string, object> args)
-        {
-            _args = args;
-            Stopwatch = new Stopwatch();
-        }
-        public Stopwatch Stopwatch { get; set; }
-        public void Start()
-        {
-            Stopwatch.Reset();
-            Stopwatch.Start();
-        }
-
-        public void Stop()
-        {
-            Stopwatch.Stop();
-            Log();
-        }
-
-        public void Log()
-        {
-            var builder = new StringBuilder();
-            builder.Append(string.Join(";", _args.Select(r => $" {r.Key}-{r.Value}")));
-            builder.Append($"Timer: {Stopwatch.ElapsedMilliseconds} seconds");
-            App.AddTimeStamp(builder.ToString());
-            
         }
     }
 }
---
+++ Monitoring.Revit/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Serilog;
+
+namespace Monitoring.Revit
+{
+
+    public interface ITimer
+    {
+        Stopwatch Stopwatch { get; set; }
+        void AddArgs(string key, object value);
+        void Start();
+        void Stop();
+        void LogTime();
+    }
+    public class Timer : ITimer
+    {
+        private Dictionary<string, object> _args;
+
+        public Timer(Dictionary<string, object> args)
+        {
+            _args = args;
+            Stopwatch = new Stopwatch();
+        }
+        public Stopwatch Stopwatch { get; set; }
+        public void AddArgs(string key, object value)
+        {
+            if (_args == null)
+                _args = new Dictionary<string, object>();
+
+            _args.Add(key, value);
+        }
+
+        public void Start()
+        {
+            Stopwatch.Reset();
+            Stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            Stopwatch.Stop();
+            _args.Add("Opening Time", Stopwatch.ElapsedMilliseconds/1000);
+            LogTime();
+        }
+
+        public void LogTime()
+        {
+            Log.Information("Revit Timer {Data}", _args);
+        }
+    }
+}
